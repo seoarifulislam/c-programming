077cb12040a23a4164fc6a211d4024d5150da7ae
Md. Ariful Islam <nbhackbook@gmail.com>
2023-04-05 11:29:07 +0600

    Module 2.5 Practice Day 01

---
+++ Module 2.5 Practice Day 01/PracticeP_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+
+{
+    int a=10, b=3;
+    int sum= a+b;
+    int minus= a-b;
+    int into= a*b;
+    float dvd= a*1.0/b;
+    printf("%d %d %d %0.2f",sum,minus,into,dvd);
+  
+   
+    return 0;
+}
