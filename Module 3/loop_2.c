f34586956919172fdaa80bb378f0d67393f2d153
Md. Ariful Islam <nbhackbook@gmail.com>
2023-04-05 21:34:23 +0600

    Module 3

---
+++ Module 3/loop_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+
+{
+
+    int i;
+    for(i=20;i>=1;i=i-3)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
---
+++ Module 3/loop_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+
+{
+
+    long long int sum=0;
+    int i;
+    int n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i=i+1)
+    {
+        sum+=i;
+    }
+    printf("%lld\n",sum);
+    return 0;
+}
